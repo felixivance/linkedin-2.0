e72d95dee06790b3a64b07c57da69c2d4e794a4c
Felix <felixrunye@gmail.com>
2022-01-18 17:07:40 +0300

    updated middleware function to redirect if not authenticated

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,16 +1,16 @@
-import { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
-export async function middlware(req) {
-    if (req.nexturl.pathname === '/') {
+export async function middleware(req) {
+    if (req.nextUrl.pathname === "/") {
         const session = await getToken({
             req,
             secret: process.env.JWT_SECRET,
-            secureCookie: process.env.NODE_ENV === 'production'
+            secureCookie: process.env.NODE_ENV === "production",
         });
-        //you can check for anyproperty on the session object
-        // like role=== 'admin' or name === 'john doe'
-
+        // You could also check for any property on the session object,
+        // like role === "admin" or name === "John Doe", etc.
         if (!session) return NextResponse.redirect("/home");
+        // If user is authenticated, continue.
     }
 }
