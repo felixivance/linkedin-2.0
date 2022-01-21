9fe063113cee9e13d3feb1fd3fa46bd597102d03
Felix <felixrunye@gmail.com>
2022-01-21 11:26:00 +0300

    header js added

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <Header>
+            {/* left */}
+            <div className="flex items-center space-x-2 max-w-xs">
+
+            </div>
+            {/* right */}
+            <div>
+
+            </div>
+        </Header>
+    )
+}
+
+
+export default Header;
