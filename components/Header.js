4d88107366724b15a2a65fe5ad173655ed1ee9ad
Felix <felixrunye@gmail.com>
2022-01-21 23:38:05 +0300

    added logo on header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
+import Image from "next/image";
+
 function Header() {
     return (
         <Header>
             {/* left */}
             <div className="flex items-center space-x-2 max-w-xs">
-
+                <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
             </div>
             {/* right */}
             <div>
+
 
             </div>
         </Header>
