7523c088e217778087a5b66686b2e517c807e8fa
Felix <felixrunye@gmail.com>
2022-04-04 16:15:45 +0300

    added ui functionality

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 function Header() {
     return (
         <Header>
-            {/* left */}
+            {/* left section  */}
             <div className="flex items-center space-x-2 max-w-xs">
                 <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
             </div>
-            {/* right */}
+            {/* right section */}
             <div>
 
 
